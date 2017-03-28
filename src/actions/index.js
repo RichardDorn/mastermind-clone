f7af7a28b1ce348f20eec4936464f4e769bd3353
Richard Dorn <Richard Dorn>
2017-03-27 21:27:48 -0500

    Remove irrelevent comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,6 @@ export const GUESS_SUBMITTED = 'GUESS_SUBMITTED';
 export const NEW_GAME = 'NEW_GAME';
 
 export function submitGuess(guess) {
-    //selectBook is an action creator, it needs to return an action, an object with a type property.
     return {
         type: GUESS_SUBMITTED,
         payload: guess
