eb4f8aec5da50f08f619fb9f76c525492caf5654
Richard Dorn <richard@gigavate.com>
2017-04-11 20:02:19 -0500

    Components now update when url changes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, Route, IndexRoute } from 'react-router';
 import reducers from './reducers';
-import routes from './routes';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import App from './components/app';
+import Home from './components/home';
+import Game from './components/game';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <MuiThemeProvider>
-      <Router history={browserHistory} routes={routes} />
+      <Router history={browserHistory}>
+        <Route path="/" component={App} >
+          <IndexRoute component={Home} />
+        </Route>
+        <Route path="game" component={Game} />
+      </Router>
     </MuiThemeProvider>
   </Provider>
   , document.querySelector('.container'));
